ada1736520291f5f04ce475bd24f6180b71ccb67
Gowri Shankar <shankar4112004@gmail.com>
2025-01-20 20:24:44 +0530

    Feat:completed the user model ,controller and cookie generation for session management

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
-
-import { signup } from "../controller/auth.controller.js";
+import {protectRoute} from "../middleware/protectRoute.js";
+import { signup ,login,logout,getMe} from "../controller/auth.controller.js";
 const router = express.Router();
-router.get("/signup", signup)
-
+router.post("/signup", signup)
+router.post("/login", login)
+router.post("/logout", logout)
+router.get("/me",protectRoute,getMe);
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
 import authRoute from "./routes/auth.route.js";
-
+import connectDB from "./db/connectDB.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Use a default port if not defined in .env
-
+app.use(express.json())
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is deployed at ${PORT}`);
+    connectDB();
 });
